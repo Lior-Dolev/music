7f9ae8d8f4d53b9f4e09f95bf2a93e1b1c420bc1
Lior Dolev <liordolev7@gmail.com>
2020-04-21 09:41:28 +0300

    change video url

--- static/Albums/Meshakech_Keevim/01/index.tsx
+++ static/Albums/Meshakech_Keevim/01/index.tsx
@@ -4,5 +4,5 @@ export default {
   Name: 'משכך כאבים',
   TrackNumber: 1,
   Lyrics: Lyrics,
-  VideoLink: 'https://www.youtube.com/embed/bpOSxM0rNPM'
+  VideoLink: 'https://www.youtube.com/embed/-D9QZ0yFRS0'
 }
